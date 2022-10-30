e8858fe84cbab50ce55512fb2c3d29d24b39e912
jose_pro <sejoma989@gmail.com>
2022-10-29 20:18:51 -0500

    feat: listing income and expenses dinamic w/ js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 const ingresos = [
-    new Ingreso('Salario', 2100.00),
+    new Ingreso('Salario', 2200.00),
     new Ingreso('Venta coche', 1500.00),
 ];
 
 const egresos = [
     new Egreso('Renta departamento', 900),
-    new Egreso('Ropa', 400)
+    new Egreso('Ropa', 600)
 ];
 
 let cargarApp = () => {
     cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
 }
 
 let totalIngresos = () => {
@@ -33,7 +35,7 @@ let cargarCabecero = () => {
     let porcentajeEgreso = totalEgresos() / totalIngresos();
     // console.log(totalIngresos());
     // console.log(totalEgresos());
-    console.log(porcentajeEgreso);
+    // console.log(porcentajeEgreso);
     document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
     document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
     document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
@@ -53,4 +55,57 @@ let formatoPorcentaje = ( valor ) => {
         style:'percent',
         minimumFractionDigits:2
     });
+}
+
+const cargarIngresos = () => {
+    let ingresosHTML = '';
+    for (let ingreso of ingresos){
+        ingresosHTML += crearIngresoHTML(ingreso);
+    }
+
+    document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
+}
+
+const crearIngresoHTML = ( ingreso ) => {
+    let ingresoHTML = `
+    <div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${ingreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">+ ${formatoMoneda(ingreso.valor)}</div>
+        <div class="elemento_eliminar">
+            <button class="elemento_eliminar--btn">
+                <ion-icon name="close-circle-outline"></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>  
+    `;
+    return ingresoHTML;
+}
+
+const cargarEgresos = () => {
+    let egresosHTML = '';
+    for (let egreso of egresos){
+        egresosHTML += crearEgresoHTML( egreso );
+    }
+
+    document.getElementById('lista-egresos').innerHTML = egresosHTML;
+}
+
+const crearEgresoHTML = ( egreso ) => {
+    let egresoHTML = `
+    <div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${egreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">- ${formatoMoneda(egreso.valor)}</div>
+        <div class="elemento_porcentaje">${formatoPorcentaje( egreso.valor / totalEgresos() )}</div>
+        <div class="elemento_eliminar">
+            <button class="elemento_eliminar--btn">
+                <ion-icon name="close-circle-outline"></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>
+    `;
+    return egresoHTML;
 }
